be001ce780c001d73135c907147311ba891c745c
Todd Johnston <tjortho@msn.com>
2012-07-28 14:07:51 -0500

    Added error checking for sytem library version

--- lib/RFID.rb
+++ lib/RFID.rb
@@ -1,5 +1,23 @@
 require "RFID/version"
+require "ffi"
 
 module RFID
-  # Your code goes here...
+  extend FFI::Library
+  ffi_lib "libnfc"  
+
+  ###
+  #Version checking before wrapping the rest of the library to prevent
+  #name method matching errors.
+  LIBNFC_SYSTEM_VERSION = "1.6.0-rc1"
+  attach_function :nfc_version, [], :string
+
+  #Fail if wrong version returned and go no further
+  if nfc_version !~ (Regexp.new(LIBNFC_SYSTEM_VERSION))
+    raise LoadError, 
+      "Error connecting to the libnfc library
+      Your system version of libnfc does not match!
+      Requires version: #{LIBNFC_SYSTEM_VERSION}"
+  end
+
+
 end
